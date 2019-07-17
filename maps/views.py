64019bc765911592e2981b417450cf2c163faa03
ChaewonShin <chaewonshin95@gmail.com>
2019-07-17 15:20:29 +0900

    mainpage & base 연결 & url 생성

--- maps/views.py
+++ maps/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request):
-    return render(request, 'base.html')
+def main(request):
+    return render(request, 'maps/MainPage.html')
