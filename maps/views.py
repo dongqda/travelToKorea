4e5f1fee953ffe971d2ed503bbbeddbad25365ab
jeong369 <injeong319@gmail.com>
2019-07-17 15:18:31 +0900

    detailpage path 설정

--- maps/views.py
+++ maps/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+# detail page
+def detailpage(request):
+    return render(request, 'maps/DetailPage.html')
