7d89740ca6d9f50d3b9881cc461900de000f3e7e
ChaewonShin <chaewonshin95@gmail.com>
2019-07-19 16:38:37 +0900

    회원가입 & 로그인 & 로그아웃 기능 구현/ 추후 모달로 구현 예정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('mypage/', views.mypage, name="mypage"),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('<str:username>/', views.mypage, name='mypage')
     ]
-
